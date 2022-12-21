abc509f1ee48d753f9ed6a6433951e7db4b60edd
PsalmSevenEleven <73731526+PsalmSevenEleven@users.noreply.github.com>
2022-12-21 14:39:18 -0500

    Initialize main

---
+++ CollectAThon/Source/CollectAThon/Public/BP_FuncLib_VectorMath.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "BP_FuncLib_VectorMath.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COLLECTATHON_API UBP_FuncLib_VectorMath : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+		UFUNCTION(BlueprintPure, Category = "Custom 3D Vector Operations")
+		static float GetAngleBetweenVectors(FVector a, FVector b, FVector vertex);
+		UFUNCTION(BlueprintPure, Category = "Custom 3D Vector Operations")
+		static FVector VectorAtDistanceOnLine(FVector startPoint, FVector endPoint, float distance);
+};
